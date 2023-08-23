c84ff0b997e9ec1792e53b2d475c08f8b9703d30
Arber <arber@Arbers-MBP.fritz.box>
2023-08-23 22:32:23 +0200

    learning types

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,23 @@
 "use strict";
-let lname = "John";
-lname = 10;
+let lname;
+lname = "Arber";
+let newname = lname.toUpperCase();
+let age;
+age = 25;
+age = 25.5;
+let dob = "25";
+let result = parseInt(dob);
+let isValid = false;
+console.log(isValid);
+let empList;
+empList = ["Arber", "Arber1", "Arber2"];
+let numList;
+numList = [1, 2, 3, 4, 5];
+let results = numList.filter((num) => num > 2);
+let emp = empList.find((emp) => emp === "Arber");
+let sum = numList.reduce((acc, num) => acc + num);
+console.log(results);
+console.log("employer:", emp);
+console.log('sum', sum);
+let c = 2 /* Color.Blue */;
+
--- datatypes.ts
+++ datatypes.ts
@@ -1,3 +1,62 @@
-let lname = "John";
+let lname : string;
 
-// lname = 10;
+ lname = "Arber";
+
+ let newname = lname.toUpperCase()
+
+ let age: number;
+
+ age = 25;
+ age = 25.5;
+
+ let dob = "25"
+ 
+ let result = parseInt(dob)
+
+ let isValid : boolean = false;
+
+ console.log(isValid);
+
+ let empList :string[];
+ empList = ["Arber", "Arber1", "Arber2"]
+
+ let numList : Array<number>;
+
+numList = [1,2,3,4,5];
+ 
+let results = numList.filter( (num) => num > 2 )
+
+let emp = empList.find( (emp) => emp ==="Arber")
+
+let sum = numList.reduce( (acc, num) =>  acc + num )
+
+console.log(results);
+console.log("employer:", emp);
+
+console.log('sum', sum);
+
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+let c: Color = Color.Blue
+
+// tuple-s
+
+let swapNumbs: [firstNumber:number,secondNumber:number];
+
+function swapNumbers(num1:number,num2:number) : [ number , number ] {
+    return [num2,num1]
+}
+
+swapNumbs = swapNumbers(10,20);
+
+swapNumbs[0];
+swapNumbs[1];
+
+// any 
+
+let departament:any;
