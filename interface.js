71badef68dc0eeb979b5f604da07b5f0350be91d
Arber <arber@Arbers-MBP.fritz.box>
2023-08-26 23:24:04 +0200

    interfaces

--- interface.js
+++ interface.js
@@ -16,15 +16,26 @@ let users = [
     { name: "Arber", id: 2 },
     { name: "Beni", id: 3, email: "" },
 ];
-// to desstructure the array we do like this: 
+// to desstructure the array we do like this:
 let [user1, user2, ...restUsers] = [
     { name: "John", id: 1, email: "" },
     { name: "Arber", id: 2, email: "" },
-    { name: "Beni", id: 3, email: "" }, { name: "Beni", id: 3, email: "" },
     { name: "Beni", id: 3, email: "" },
     { name: "Beni", id: 3, email: "" },
-    { name: "Beni", id: 3, email: "" },
+    { name: "Beni", id: 4, email: "" },
+    { name: "Beni", id: 5, email: "" },
+    { name: "Beni", id: 6, email: "" },
 ];
 console.log("user 1:", user1);
 console.log("user 2:", user2);
 console.log("the rest of users:", restUsers);
+// filter method
+// let result = restUsers.filter(user => user.id > 3);
+// INTERFACES do not exist in java script
+// DO not uses the interfaces in Three.js
+// BETTER TO USE CLASSES
+// Decorators are used internally by Angular 
+// @Component({}) // the Compoent is a decorator, 
+// class Component {
+//     constructor(public name:string) {}
+// }
--- interface.ts
+++ interface.ts
@@ -50,20 +50,37 @@ let users: User[] = [
   { name: "Beni", id: 3, email: "" },
 ];
 
-// to desstructure the array we do like this: 
+// to desstructure the array we do like this:
 
-let [user1,user2, ...restUsers]: User[] = [
-    { name: "John", id: 1, email: "" },
-    { name: "Arber", id: 2 ,email:""},
-    { name: "Beni", id: 3, email: "" },    { name: "Beni", id: 3, email: "" },
-    { name: "Beni", id: 3, email: "" },
-    { name: "Beni", id: 3, email: "" },
-    { name: "Beni", id: 3, email: "" },
+let [user1, user2, ...restUsers]: User[] = [
+  { name: "John", id: 1, email: "" },
+  { name: "Arber", id: 2, email: "" },
+  { name: "Beni", id: 3, email: "" },
+  { name: "Beni", id: 3, email: "" },
+  { name: "Beni", id: 4, email: "" },
+  { name: "Beni", id: 5, email: "" },
+  { name: "Beni", id: 6, email: "" },
+];
 
-  ];
+console.log("user 1:", user1);
+console.log("user 2:", user2);
+console.log("the rest of users:", restUsers);
 
-  console.log("user 1:",user1);
-  console.log("user 2:",user2);
-  console.log("the rest of users:",restUsers);
-  
-  
+// filter method
+
+// let result = restUsers.filter(user => user.id > 3);
+
+// INTERFACES do not exist in java script
+// DO not uses the interfaces in Three.js
+
+// BETTER TO USE CLASSES
+
+
+// Decorators are used internally by Angular 
+
+// @Component({}) // the Compoent is a decorator, 
+// class Component {
+//     constructor(public name:string) {}
+// }
+
+// TYPE script can stop you from compiling
