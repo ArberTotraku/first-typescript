7ba0d8942359ce0602cc90e50b6ab11de882284e
Arber <arber@Arbers-MBP.fritz.box>
2023-08-29 21:46:38 +0200

    ngIf

--- hotelinventory/src/app/rooms/rooms.component.ts
+++ hotelinventory/src/app/rooms/rooms.component.ts
@@ -1,19 +1,52 @@
 import { Component } from '@angular/core';
-
+import { Room } from './rooms';
+import { RoomList } from './rooms';
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.sass']
+  styleUrls: ['./rooms.component.sass'],
 })
 export class RoomsComponent {
+  hotelName = 'Hilton hotel';
 
-
-  hotelName = "Hilton hotel";
-
-  numberOfRooms= 10;
+  numberOfRooms = 10;
 
   hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomType: 'Deluxe Room',
+      amenities: 'Air conditioner, Free wifi,Tv,Bathroom,Kitchen',
+      price: 500,
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      photos: 'https://unsplash.com/photos/3jctzdjyTas',
+    },
+    {
+      roomType: 'Deluxe Room',
+      amenities: 'Air conditioner, Free wifi,Tv,Bathroom,Kitchen',
+      price: 1000,
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      photos: 'https://unsplash.com/photos/3jctzdjyTas',
+    },
+    {
+      roomType: 'Private Room',
+      amenities:'Air conditioner, Free wifi,Tv,Bathroom,Kitchen',
+      price:15000,
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      photos:'https://unsplash.com/photos/3jctzdjyTas'
+    },
+  ];
+
   toggle() {
-    this.hideRooms = !this.hideRooms
+    this.hideRooms = !this.hideRooms;
   }
 }
